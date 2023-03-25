ced381e27a45fd9545925d78157a0173ff1f4585
diawando <abduldiawade777@gmail.com>
2023-03-25 03:04:37 +0000

    ajout de la fonction create_app, et create_database()

--- src/__init__.py
+++ src/__init__.py
@@ -7,3 +7,18 @@ from .blog import blog
 load_dotenv()
 db = SQLAlchemy()
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://os.getenv("DB_USER"):os.getenv("DB_PASSWORD")@localhost:5432/rbac_blog"
+    db.init_app(app)
+    
+    create_database(app)
+    
+    app.register_blueprint(blog, url_prefix="")
+    return app
+
+def create_database(app):
+     with app.app_context():
+         db.create_all()
+     print("Database created")
