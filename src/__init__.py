9ce815b50be9d4319fe7aa58ef88b4ddb4d19c76
diawando <abduldiawade777@gmail.com>
2023-03-26 03:21:18 +0000

    mis à jour du fichier __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 from .blog import blog
+from flask_login import LoginManager
+from .auth import auth
+
 
 load_dotenv()
 db = SQLAlchemy()
@@ -15,7 +18,18 @@ def create_app():
     
     create_database(app)
     
+    login_manager = LoginManager() # handless session for users
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+    
+    
+    @login_manager.user_loader
+    def load_user(id):
+        with app.app_context():
+            return User.query.get(int(id))
+    
     app.register_blueprint(blog, url_prefix="")
+    app.register_blueprint(auth, url_prefix="/auth")
     return app
 
 def create_database(app):
